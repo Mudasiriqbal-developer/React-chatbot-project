6427293e491b68a2fa082e5ae7ea3ae45378de77
Mudasir-Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-01-08 14:36:30 +0500

    Uplaod chatbot project on the github

---
+++ ChatBot-Project/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState, useRef, useEffect} from 'react';
+import { ChatInput } from './components/ChatInput';
+import  ChatMessages  from './components/ChatMessages';
+import RobotProfileImage from './assets/robot.png';
+import './App.css'
+
+export function useAutoScroll(dependencies) {
+
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const containerElem = containerRef.current;
+    if(containerElem) {
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }
+  }, dependencies);
+
+  return containerRef;
+}
+
+function App() {
+
+  const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem('messages')) || []);
+
+  useEffect(() => {
+    localStorage.setItem('messages', JSON.stringify(chatMessages));
+  }, [chatMessages]);
+  
+  const title = `${chatMessages.length} Megs`;
+  return (
+    
+    <>
+      <title>ChatBot: {title}</title>
+      <link rel="icon" type="image/svg+xml" href={RobotProfileImage} />
+
+      <div className="app-container">
+        <ChatMessages
+          chatMessages={chatMessages}
+        />
+        <ChatInput
+          chatMessages={chatMessages}
+          setChatMessages={setChatMessages}
+        />
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ ChatBot-Project/src/components/ChatInput.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+import  dayjs  from 'dayjs';
+import { Chatbot } from 'supersimpledev';
+import LaodingSpinImage from '../assets/loading-spinner.gif'
+
+import './ChatInput.css';
+
+export function ChatInput({chatMessages, setChatMessages}) {
+  const [inputText, setInputText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  function saveInputText(event) {
+    setInputText(event.target.value);
+  }
+
+    async function sendMessage() {
+    if (isLoading || inputText === '') {
+      return;
+    }
+    setIsLoading(true);
+
+    setInputText('');
+
+    const newChatMessages = [
+      ...chatMessages,
+      {
+        message: inputText,
+        sender: 'user',
+        is: crypto.randomUUID(),
+        time:dayjs().valueOf()
+      }
+    ];
+    setChatMessages([
+      ...newChatMessages,
+
+      {
+        message: <img src={LaodingSpinImage} className="loading-spinner" />,
+        sender: 'robot',
+        id: crypto.randomUUID(),
+        time:dayjs().valueOf()
+      }
+    ]);
+
+    const response =await Chatbot.getResponseAsync(inputText);
+      setChatMessages([
+      ...newChatMessages,
+      {
+        message: response,
+        sender: 'robot',
+        is: crypto.randomUUID()
+      }
+    ]);
+    setIsLoading(false)
+  }
+
+  function handleKeyDown(event) {
+    if(event.key === 'Enter') {
+      sendMessage();
+    }
+  }
+
+  function clearMessages() {
+    setChatMessages([]);
+
+    // Here, you could also run:
+    // localStorage.setItem('messages', JSON.stringify([]));
+
+    // However, because chatMessages is being updated, the
+    // useEffect in the App component will run, and it will
+    // automatically update messages in localStorage to be [].
+  }
+
+  return (
+    <div className="chat-input-container">
+      <input  
+        disabled={isLoading}
+        placeholder="Send Message To ChatBot" 
+        size="30" 
+        onChange={saveInputText}
+        onKeyDown={handleKeyDown}
+        value={inputText}
+        className="chat-input"
+      />
+      <button
+        onClick={sendMessage}
+        className="send-button"
+      >Send
+      </button>
+      <button
+        onClick={clearMessages}
+        className="clear-button"
+      >Clear
+      </button>
+      
+    </div>
+  );
+}
